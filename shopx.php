7a2e5a19690e72e0c2b5f815c65bd5befc9dd176
Hamza <hamzapervaiz5@gmail.com>
2020-05-31 10:46:06 +0500

    automotive shop

--- shopx.php
+++ shopx.php
@@ -10,7 +10,7 @@
 
 	<!-- Document Wrapper
 		============================================= -->
-	<div id="wrapper" class="clearfix">
+		<div id="wrapper" class="clearfix">
 
 		<!-- Header
 			============================================= -->
@@ -73,7 +73,7 @@
 				</div></div></div><ol class="flex-control-nav flex-control-paging"><li><a href="#" class="flex-active">1</a></li><li><a href="#">2</a></li><li><a href="#">3</a></li><li><a href="#">4</a></li></ol><ul class="flex-direction-nav"><li class="flex-nav-prev"><a class="flex-prev" href="#"><i class="icon-angle-left"></i></a></li><li class="flex-nav-next"><a class="flex-next" href="#"><i class="icon-angle-right"></i></a></li></ul></div>
 			</div>
 
-	</div>
+		</div>
 
 	</section>
 
@@ -108,80 +108,139 @@
 
 	</div>
 </div>
-<div class="row">
-	<div class="col-md-12 text-center">
-
-		<h4>Features:</h4>
-
-	</div>
-
-	<div class="col-md-6">
-
-
-		<div style="margin-left: 20px">
-			<a href="shop/design-clothing/">
-				<h5>Customer Level</h5>
-			</a>
-			<ol>
-				<li><span>Highly Customizable according to Clients and Customers Taste.</span></li>
-				<li><span>Responsive View on Mobile, Tablet, Laptop or Any Large Screen Display.</span></li>
-				<li><span>Products Levels:&nbsp; Main Category &gt; Sub Category &gt; Products.</span></li>
-				<li><span>Sale and Current Price Management.</span></li>
-				<li><span>Brands and Clietns Showcase.</span></li>
-				<li><span>Cart Products and Details View on all Pages.</span></li>
-				<li>Cart Management, Shipping Details, Billing Details.</li>
-				<li><span>Public Checkout Options. (No Login/Signup is Required).</span></li>
-				<li><span>Cash on Delivery, JassCash/Easypaisa and Debit Card Checkout.</span></li>
-			</ol>
-
-		</div>
-
-
-
-	</div>
-	<div class="col-md-6">
-
-
-		<div style="margin-left: 20px">
-
-			<a href="shop/design-clothing/admin757">
-				<h5>Admin Level</h5>
-			</a>
-			<ol>
-				<li><span>Website &amp; Homepage Customization</span></li>
-				<li><span>Products, Category, SubCategoty Management</span></li>
-				<li><span>Complete Order Management:</span></li>
-				<ul style="margin-left: 20px">
-					<li>Orders Currently in Customers Cart</li>
-					<li>Pending Orders</li>
-					<li>Orders Dispatched (in Delivery)</li>
-					<li>Delivered/Completed Orders</li>
-					<li>Expired/Rejected Orders</li>
-				</ul>	
-				<li>Orders  Searching By:</li>
-				<ul style="margin-left: 20px">
-					<li>Order ID , Customer's Name , Order Status , Order Price , Order Date</li>
-				</ul>
-				<li>Invoice Creation and Printing</li>
-
-
-			</ol>
-		</div>
-	</div>
-
-</div>
-</div>
 
 
 <hr>
 
 <br><br>
-			<div class="container">
-				<div class="text-center">
-					<h2 style="color: #125484">Online Appliances Shop Demo </h2>
+<div class="container">
+	<div class="text-center">
+		<h2 style="color: #da3100">Automotive Online Shop Demo </h2>
+	</div>
+	<div class="row"  style="margin-bottom: 100px">
+		<div class="col-md-3 topmargin-lg">
+
+			<h3 style="color: #da3100">Automotive  Store</h3>
+			<button class="tbtn" style="border: 1px solid #da3100; color: #da3100; background: transparent;"> WordPress </button> &nbsp; <button class="tbtn" style="border: 1px solid #da3100; color: #da3100; background: transparent;"> WooCommerce </button>
+			<br>
+			<br>
+
+			<p>Your choice is an inner expression of yours,
+					So choose the best with the best.
+
+					</p>
+
+				<p><span style="font-size: 0.875rem;">We also have a full range of Demo cars to suit your best needs and can be altered according to your choice and need.
+
+				</span></p>
+
+
+				<a target="blank" href="https://demo.wilcode.com/shop/design-automotive/" class="tbtn" style="background: #da3100;color: white;border-radius: 1px; padding: 5px">
+					Customer Demo
+				</a> &nbsp; 
+
+				<a target="blank" href="https://demo.wilcode.com/shop/design-automotive/wp-admin" class="tbtn" style="background: #da3100;color: white;border-radius: 1px; padding: 5px">
+					Admin Demo
+				</a>
+
+			</div>
+			<div class="col-md-9 text-center " >
+
+				<section id="slider" class="slider-element boxed-slider"><a href="shop/design-clothing/">
+
+				</a><div class="container clearfix"><a href="shop/design-clothing/">
+
+				</a><div class="fslider" data-easing="easeInQuad" style="width: 100%"><a href="shop/design-clothing/">
+				</a><div class="flexslider"><a href="shop/design-clothing/">
+				</a>
+				<div class="flex-viewport" style="overflow: hidden; position: relative;"><div class="slider-wrap" style="width: 1200%; margin-left: -759px;">
+
+					<div class="slide clone" aria-hidden="true" style="width: 100%; margin-right: 0px; float: left; display: block;">
+					<a href="#">
+						<img src="images/demo/shop/auto/1.png" alt="Slide 5" draggable="false">
+						<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-bottom-right">Ecommerce Appliance Site</div>
+					</a>
 				</div>
-				<div class="row">
-					<div class="col-md-3 topmargin-lg">
+
+					<div class="slide" aria-hidden="true" style="width: 100%; margin-right: 0px; float: left; display: block;">
+					<a href="#">
+						<img src="images/demo/shop/auto/2.png" alt="Slide 5" draggable="false">
+						<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-bottom-right">Ecommerce Appliance Site</div>
+					</a>
+				</div>
+
+
+			</div></div><ul class="flex-direction-nav"><li class="flex-nav-prev"><a class="flex-prev" href="#"><i class="icon-angle-left"></i></a></li><li class="flex-nav-next"><a class="flex-next" href="#"><i class="icon-angle-right"></i></a></li></ul></div>
+		</div>
+
+	</div>
+
+</section>
+
+<br>
+<br>
+</center>
+
+
+</div>
+
+</div>
+
+
+
+<hr>
+
+<br><br>
+<div class="container">
+	<div class="text-center">
+		<h2 style="color: #125484">Online Appliances Shop Demo </h2>
+	</div>
+	<div class="row">
+
+		<div class="col-md-8 text-center">
+
+			<section id="slider" class="slider-element boxed-slider"><a href="shop/design-clothing/">
+
+			</a><div class="container clearfix"><a href="shop/design-clothing/">
+
+			</a><div class="fslider" data-easing="easeInQuad" style="width: 100%"><a href="shop/design-clothing/">
+			</a><div class="flexslider"><a href="shop/design-clothing/">
+			</a>
+			<div class="flex-viewport" style="overflow: hidden; position: relative;"><div class="slider-wrap" style="width: 1200%; margin-left: -759px;"><div class="slide clone" aria-hidden="true" style="width: 759px; margin-right: 0px; float: left; display: block;">
+				<a href="#">
+					<img src="images/demo/shop/appliances/1.jpg" alt="Slide 5" draggable="false">
+					<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-bottom-right">Ecommerce Appliance Site</div>
+				</a>
+			</div>
+
+
+			<div class="slide" data-thumb-alt="" style="width: 759px; margin-right: 0px; float: left; display: block;">
+				<a href="#">
+					<img src="images/demo/shop/appliances/2.jpg" alt="Slide 5" draggable="false">
+					<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-bottom-right">Elegant Online Appliances Shop </div>
+				</a>
+			</div>
+			<div class="slide clone" style="width: 759px; margin-right: 0px; float: left; display: block;" aria-hidden="true"><a href="shop/design-clothing/">
+			</a><a href="#">
+				<img src="images/demo/shop/appliances/3.jpg" alt="Slide 2" draggable="false">
+				<div class="flex-caption slider-caption-bg  slider-caption-bottom-right">Mobile Friendly Design</div>
+			</a>
+		</div>
+	</div>
+</div>
+<ul class="flex-direction-nav"><li class="flex-nav-prev"><a class="flex-prev" href="#"><i class="icon-angle-left"></i></a></li><li class="flex-nav-next"><a class="flex-next" href="#"><i class="icon-angle-right"></i></a></li></ul>
+</div>
+</div>
+
+
+
+
+</div>
+
+
+</section>
+</div>
+	<div class="col-md-4 topmargin-sm">
 
 	<h3 style="color: #125484">Shop Appliances</h3>
 	<button class="tbtn" style="border: 1px solid #125484; color: #125484; background: transparent;"> Custom </button> &nbsp; <button class="tbtn" style="border: 1px solid #125484; color: #125484; background: transparent;"> PHP/Laravel </button>
@@ -203,53 +262,27 @@
 		</a>
 
 	</div>
-					<div class="col-md-9 text-center">
 
-						<section id="slider" class="slider-element boxed-slider"><a href="shop/design-clothing/">
-
-						</a><div class="container clearfix"><a href="shop/design-clothing/">
-
-						</a><div class="fslider" data-easing="easeInQuad" style="width: 100%"><a href="shop/design-clothing/">
-						</a><div class="flexslider"><a href="shop/design-clothing/">
-						</a>
-						<div class="flex-viewport" style="overflow: hidden; position: relative;"><div class="slider-wrap" style="width: 1200%; margin-left: -759px;"><div class="slide clone" aria-hidden="true" style="width: 759px; margin-right: 0px; float: left; display: block;">
-							<a href="#">
-								<img src="images/demo/shop/appliances/1.jpg" alt="Slide 5" draggable="false">
-								<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-bottom-right">Ecommerce Appliance Site</div>
-							</a>
-						</div>
-
-
-					<div class="slide" data-thumb-alt="" style="width: 759px; margin-right: 0px; float: left; display: block;">
-						<a href="#">
-							<img src="images/demo/shop/appliances/2.jpg" alt="Slide 5" draggable="false">
-							<div class="flex-caption slider-caption-bg slider-caption-bg-light slider-caption-bottom-right">Elegant Online Appliances Shop </div>
-						</a>
-					</div>
-					<div class="slide clone" style="width: 759px; margin-right: 0px; float: left; display: block;" aria-hidden="true"><a href="shop/design-clothing/">
-					</a><a href="#">
-						<img src="images/demo/shop/appliances/3.jpg" alt="Slide 2" draggable="false">
-						<div class="flex-caption slider-caption-bg  slider-caption-bottom-right">Mobile Friendly Design</div>
-					</a>
-				</div></div></div><ul class="flex-direction-nav"><li class="flex-nav-prev"><a class="flex-prev" href="#"><i class="icon-angle-left"></i></a></li><li class="flex-nav-next"><a class="flex-next" href="#"><i class="icon-angle-right"></i></a></li></ul></div>
-			</div>
-
-	</div>
-
-	</section>
-
-	<br>
-	<br>
+<br>
+<br>
 </center>
 
 
 </div>
 
 </div>
+</div>
+</div>
+<br>
+<br>
+
+<hr>
+<br>
+<div class="container">
 <div class="row">
 	<div class="col-md-12 text-center">
 
-		<h4 style="color: #125484;">Features:</h4>
+		<h2 style="color: #000;">Online Shop Features:</h2>
 		<br>
 
 	</div>
@@ -259,7 +292,7 @@
 
 		<div style="margin-left: 20px">
 			<a href="shop/design-clothing/">
-				<h5 style="color: #125484;">Customer Level</h5>
+				<h5 style="color: #000;">Customer Level</h5>
 			</a>
 			<ol>
 				<li><span>Highly Customizable according to Clients and Customers Taste.</span></li>
@@ -290,7 +323,7 @@
 		<div style="margin-left: 20px">
 
 			<a href="shop/design-clothing/admin757">
-				<h5 style="color: #125484;">Admin Level</h5>
+				<h5 style="color: #000;">Admin Level</h5>
 			</a>
 			<ol>
 				<li><span>Website &amp; Homepage Customization</span></li>
@@ -317,6 +350,8 @@
 </div>
 </div>
 
+<br>
+<br>
 
 
 
